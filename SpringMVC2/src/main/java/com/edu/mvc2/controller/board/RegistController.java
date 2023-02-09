bd9d6130b821ababc0b4e29269e62a54d77306fd
zino <zino1187@gmail.com>
2023-02-09 14:27:45 +0900

    first

--- SpringMVC2/src/main/java/com/edu/mvc2/controller/board/RegistController.java
+++ SpringMVC2/src/main/java/com/edu/mvc2/controller/board/RegistController.java
@@ -6,10 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.edu.mvc2.model.board.BoardService;
+
+import lombok.Setter;
+
 //게시판의 글쓰기 요청을 처리하는 하위 컨트롤러..
 //2.x 방식
+@Setter
 public class RegistController implements Controller{
-
+	private BoardService boardService;
+	
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String title=request.getParameter("title");
 		String writer=request.getParameter("writer");
